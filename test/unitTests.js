9a0f3902a4ebcd66b0b572dbc10e07aa9d40dd85
tmaslen <tom_maslen@hotmail.com>
2019-12-26 11:14:08 +0000

    Reorder tests to be more obvious.

--- test/unitTests.js
+++ test/unitTests.js
@@ -17,61 +17,6 @@ afterEach(() => {
 
 describe( "F2T", () => {
 
-	it( "get() method", () => {
-
-		const f2t = new F2T( {}, {
-			"trueBoolean": true,
-			"falseBoolean": false,
-			"number": 33,
-			"array": [ 1, 2 ],
-			"object": { "foo": "bar" },
-			"text": "laserwolf"
-		});
-
-		expect( f2t.get( "notDefined" )   ).to.equal( null );
-		expect( f2t.get( "trueBoolean" )  ).to.equal( true );
-		expect( f2t.get( "falseBoolean" ) ).to.equal( false );
-		expect( f2t.get( "number" )       ).to.equal( 33 );
-		expect( f2t.get( "array" )        ).to.deep.equal( [ 1, 2 ] );
-		expect( f2t.get( "object" )       ).to.deep.equal( { "foo": "bar" } );
-		expect( f2t.get( "text" )         ).to.equal( "laserwolf" );
-
-	});
-
-	it( "when() method", () => {
-
-		const f2t = new F2T( {}, {
-			"trueBoolean": true,
-			"falseBoolean": false,
-			"number": 33,
-			"text": "laserwolf"
-		});
-
-		f2t.when( "notDefined" )
-			//.is( f2t._anyValue, () => assert.fail() )
-			.else( () => assert.isOk( true ) );
-
-		f2t.when( "trueBoolean" )
-			.is( true, () => assert.isOk( true ) )
-			.is( false, () => assert.fail() )
-			.else( () =>  () => assert.fail() );
-
-		f2t.when( "falseBoolean" )
-			.is( true, () => assert.fail() )
-			.is( false, () => assert.isOk( true ) )
-			.else( () =>  assert.fail() );
-
-		f2t.when( "number" )
-			.is( 33, () => assert.isOk( true ) )
-			.else( () => assert.fail() );
-
-
-		f2t.when( "text" )
-			.is( "laserwolf", () => assert.isOk( true ) )
-			.else( () => assert.fail() );
-
-	});
-
 	describe( "Constructor method", () => {
 
     	it( "Sets properties required for API call", async () => {
@@ -175,6 +120,61 @@ describe( "F2T", () => {
 				});
 
 		});
+
+	});
+
+	it( "get() method", () => {
+
+		const f2t = new F2T( {}, {
+			"trueBoolean": true,
+			"falseBoolean": false,
+			"number": 33,
+			"array": [ 1, 2 ],
+			"object": { "foo": "bar" },
+			"text": "laserwolf"
+		});
+
+		expect( f2t.get( "notDefined" )   ).to.equal( null );
+		expect( f2t.get( "trueBoolean" )  ).to.equal( true );
+		expect( f2t.get( "falseBoolean" ) ).to.equal( false );
+		expect( f2t.get( "number" )       ).to.equal( 33 );
+		expect( f2t.get( "array" )        ).to.deep.equal( [ 1, 2 ] );
+		expect( f2t.get( "object" )       ).to.deep.equal( { "foo": "bar" } );
+		expect( f2t.get( "text" )         ).to.equal( "laserwolf" );
+
+	});
+
+	it( "when() method", () => {
+
+		const f2t = new F2T( {}, {
+			"trueBoolean": true,
+			"falseBoolean": false,
+			"number": 33,
+			"text": "laserwolf"
+		});
+
+		f2t.when( "notDefined" )
+			//.is( f2t._anyValue, () => assert.fail() )
+			.else( () => assert.isOk( true ) );
+
+		f2t.when( "trueBoolean" )
+			.is( true, () => assert.isOk( true ) )
+			.is( false, () => assert.fail() )
+			.else( () =>  () => assert.fail() );
+
+		f2t.when( "falseBoolean" )
+			.is( true, () => assert.fail() )
+			.is( false, () => assert.isOk( true ) )
+			.else( () =>  assert.fail() );
+
+		f2t.when( "number" )
+			.is( 33, () => assert.isOk( true ) )
+			.else( () => assert.fail() );
+
+
+		f2t.when( "text" )
+			.is( "laserwolf", () => assert.isOk( true ) )
+			.else( () => assert.fail() );
 
 	});
 
